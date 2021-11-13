04f1c3bb79b0e6ad79bc7ca0fed05d5ed4550f9c
sxt1939 <54764075+sxt1939@users.noreply.github.com>
2021-11-13 15:02:06 +0800

    为每个连接创建一个线程

--- server.cpp
+++ server.cpp
@@ -13,7 +13,34 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <pthread.h>
+
 #define MAXLNE 4096
+
+void *client_routine(void *arg)
+{ //
+	int connfd = *(int *)arg;
+	char buff[MAXLNE];
+	//printf("========waiting for client's request ========\n");
+	while (1)
+	{
+		int n = recv(connfd, buff, MAXLNE, 0);
+        if (n > 0)
+        {
+            buff[n] = '\0';
+            printf("recv msg from client: %s\n", buff);
+
+	    	send(connfd, buff, n, 0);
+        }
+        else if (n == 0)
+        {
+            close(connfd);
+			printf("client %d is disconnect\n",connfd);
+			break;
+        }
+	}
+	return NULL;
+}
 
 int main(int argc,char **argv)
 {
@@ -42,32 +69,19 @@ int main(int argc,char **argv)
         printf("listen socket error: %s(errno: %d)\n", strerror(errno), errno);
         return 0;
     }
-    struct sockaddr_in client;
-	socklen_t len = sizeof(client);
-	printf("========waiting for client connect ========\n");
-	if ((connfd = accept(listenfd, (struct sockaddr *)&client, &len)) == -1)
-	{
-		printf("accept socket error: %s(errno: %d)\n", strerror(errno), errno);
-		return 0;
-	}
 
-	printf("========waiting for client's request connfd:%d  ========\n",connfd);
 	while (1)
 	{
-		n = recv(connfd, buff, MAXLNE, 0);
-		if (n > 0)
+		struct sockaddr_in client;
+		socklen_t len = sizeof(client);
+		printf("========waiting for client connect ========\n");
+		if ((connfd = accept(listenfd, (struct sockaddr *)&client, &len)) == -1)
 		{
-			buff[n] = '\0';
-			printf("recv msg from client: %s\n", buff);
-
-			send(connfd, buff, n, 0);
+			printf("accept socket error: %s(errno: %d)\n", strerror(errno), errno);
+			return 0;
 		}
-		else if (n == 0)
-		{
-			close(connfd);
-			printf("client %d is disconnect\n",connfd);
-		}
-		//close(connfd);
+		pthread_t threadid;
+		pthread_create(&threadid, NULL, client_routine, (void*)&connfd);
 	}
 }
 
